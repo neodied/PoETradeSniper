e81f3db28fec734e08d7816caf1747d0d5c6d934
neodied <joshua.d.wilke@gmail.com>
2017-03-28 08:21:00 -0400

    Reformat to PEP8 Round time prints and add download rate Simplify item found message and timestamp

--- sniper.py
+++ sniper.py
@@ -1,52 +1,62 @@
 import requests
-import json
+# import json
 import time
 
 CHANGE_URL = 'http://api.poe.ninja/api/Data/GetStats'
 POE_API = 'http://www.pathofexile.com/api/public-stash-tabs'
 
-def getLastestChangeID():
+
+def get_latest_change_id():
     r = requests.get(CHANGE_URL)
     return r.json()['nextChangeId']
 
-def filter(item):
-    if(item['typeLine'] == 'Hubris' and 'note' in item):
+
+def flag_interesting_item(item):
+    if item['typeLine'] == 'Hubris' and 'note' in item:
         return True
     else:
         return False
 
-def formatItem(item, stash):
-    return "[{}]{}\t{}\t{}\t{}".format(time.strftime('%l:%M%p %Z on %b %d, %Y'),item["note"], stash["accountName"], stash["lastCharacterName"], item["league"])
+
+def format_item(item, stash):
+    return "[{}]{}\t{}\t{}".format(time.strftime('%H:%M:%S'), item["typeLine"], item["note"],
+                                   stash["lastCharacterName"], )
 
 
+def main():
+    print('Grabbing latest Change ID from poe.ninja')
+    change_id = get_latest_change_id()
+    print('Starting Item search starting at {}'.format(change_id))
 
-print('Grabbing latest Change ID from poe.ninja')
-changeId = getLastestChangeID();
-print('Starting Item search starting at {}'.format(changeId))
+    while True:
+        start_time = time.time()
+        try:
+            r = requests.get('{}/?id={}.gz'.format(POE_API, change_id))
+            # print(change_id)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception as e:
+            print(str(e))
+        else:
+            j = r.json()
+            size = len(r.content)
 
-while True:
-    start_time = time.time()
-    try:
-        r = requests.get('{}/?id={}.gz'.format(POE_API, changeId))
-        #print(changeId)
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except Exception as e:
-        print(str(e))
+        change_id = j['next_change_id']
 
-    j = r.json()
-    changeId = j['next_change_id']
+        download_time = round(time.time() - start_time, 3)
+        download_rate = round(size/download_time/10**6, 3)
 
-    download_time = time.time() - start_time
+        for stash in j['stashes']:
+            for item in stash['items']:
+                if flag_interesting_item(item):
+                    print(format_item(item, stash))
 
-    for stash in j['stashes']:
-        for item in stash['items']:
-            if(filter(item)):
-                print(formatItem(item, stash))
+        end_time = round(time.time() - start_time - download_time, 3)
 
-    end_time = time.time() - start_time - download_time
+        print('Download {}s; Processing {}s; Download rate {}MB/s'.format(download_time, end_time, download_rate))
 
-    print('Download took {}s; Processing took {}s'.format(download_time, end_time))
-    
-    if((end_time + download_time) < 1.0 ):
-        time.sleep(1.0 - (end_time + download_time))
+        if (end_time + download_time) < 1.0:
+            time.sleep(1.0 - (end_time + download_time))
+
+
+main()
